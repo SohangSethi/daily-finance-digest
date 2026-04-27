3deaa2cd330fab0368f23508967dec7c142dfb42
Sohang Sethi <sohangsethi@example.com>
2026-04-26 22:02:34 -0400

    Fix Yahoo Finance survey notice

--- src/lib/yahoo.ts
+++ src/lib/yahoo.ts
@@ -4,7 +4,7 @@
 // ============================================================
 
 import YahooFinance from 'yahoo-finance2';
-const yahooFinance = new YahooFinance();
+const yahooFinance = new YahooFinance({ suppressNotices: ['yahooSurvey'] });
 
 export interface MarketDataPoint {
   name: string;
